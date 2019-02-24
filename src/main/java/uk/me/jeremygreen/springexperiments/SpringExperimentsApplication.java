f1afd36a3c5d77d6803c2dc9babbce3220bb3e12
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-02-24 13:53:34 +0000

    Add Spring MVC project generated by https://start.spring.io/.

---
+++ src/main/java/uk/me/jeremygreen/springexperiments/SpringExperimentsApplication.java
@@ -0,0 +1,13 @@
+package uk.me.jeremygreen.springexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExperimentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/me/jeremygreen/springexperiments/SpringExperimentsApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.me.jeremygreen.springexperiments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringExperimentsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
