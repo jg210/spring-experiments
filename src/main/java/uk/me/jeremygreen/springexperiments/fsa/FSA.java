2ec3778f8357f641fdc1f80729bf71237ce3ec32
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-05-13 21:15:15 +0100

    Remove unused imports.

--- src/main/java/uk/me/jeremygreen/springexperiments/fsa/FSA.java
+++ src/main/java/uk/me/jeremygreen/springexperiments/fsa/FSA.java
@@ -4,9 +4,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
-import uk.me.jeremygreen.springexperiments.fsa.api.LocalAuthority;
 
 import java.util.*;
 import java.util.concurrent.Semaphore;
