379b08e73d679461d6ac9b302131e00f68db12d3
Jeremy Green <20728342+jg210@users.noreply.github.com>
2023-12-13 20:04:04 +0000

    Convert FsaAuthorities to a record.

--- src/main/java/uk/me/jeremygreen/springexperiments/fsa/FsaAuthorities.java
+++ src/main/java/uk/me/jeremygreen/springexperiments/fsa/FsaAuthorities.java
@@ -6,20 +6,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown=true)
-public class FsaAuthorities {
-
-    private final List<FsaAuthority> authorities;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record FsaAuthorities(List<FsaAuthority> authorities) {
 
     @JsonCreator
     public FsaAuthorities(
-            @JsonProperty("authorities")
-            final List<FsaAuthority> authorities) {
+            @JsonProperty("authorities") final List<FsaAuthority> authorities) {
         this.authorities = authorities;
-    }
-
-    public final List<FsaAuthority> getAuthorities() {
-        return this.authorities;
     }
 
 }
