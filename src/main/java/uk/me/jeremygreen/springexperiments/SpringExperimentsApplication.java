8d0ece9d99fb977d977c5bf69daf2696178c303b
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-01-11 14:06:03 +0000

    com.amazonaws.serverless:aws-serverless-java-container-springboot3:2.0.0-M2

--- src/main/java/uk/me/jeremygreen/springexperiments/SpringExperimentsApplication.java
+++ src/main/java/uk/me/jeremygreen/springexperiments/SpringExperimentsApplication.java
@@ -1,13 +1,19 @@
 package uk.me.jeremygreen.springexperiments;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class SpringExperimentsApplication {
+public class SpringExperimentsApplication implements WebApplicationInitializer {
 
     public static void main(final String[] args) {
         SpringApplication.run(SpringExperimentsApplication.class, args);
     }
+
+    @Override
+    public void onStartup(ServletContext servletContext) {}
 
 }
