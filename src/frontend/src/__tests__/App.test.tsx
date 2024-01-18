3bde18df829a8c1b4a12bcd579b000ea7c98f9fa
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-01-18 16:09:34 +0000

    Refactor generic page checks into function.

--- src/frontend/src/__tests__/App.test.tsx
+++ src/frontend/src/__tests__/App.test.tsx
@@ -1,6 +1,24 @@
 import { render, screen } from '@testing-library/react'
 import App from '../App';
 
+function checkBoilerplate() {
+  // heading
+  const heading = screen.getByRole("heading");
+  expect(heading).toHaveClass("App-title");
+  expect(heading).toHaveTextContent("FSA Food Hygiene Ratings");
+  // blurb
+  const blurb = screen.getByText("The information provided here is based on data from the Food Standards Agency UK Food Hygiene Rating Data API.");
+  expect(blurb).toBeInTheDocument();
+  // URLs
+  [
+    "https://ratings.food.gov.uk",
+    "https://www.food.gov.uk/terms-and-conditions"
+  ].forEach(url => {
+    const element = screen.getByText(url);
+    expect(element).toHaveAttribute("href", url);
+  });
+}
+
 describe("App", () => {
 
   it('is run with correct node version', () => {
@@ -9,21 +27,7 @@ describe("App", () => {
 
   it('renders correctly while loading', () => {
     render(<App/>);
-    const heading = screen.getByRole("heading");
-    // heading
-    expect(heading).toHaveClass("App-title");
-    expect(heading).toHaveTextContent("FSA Food Hygiene Ratings");
-    // blurb
-    const blurb = screen.getByText("The information provided here is based on data from the Food Standards Agency UK Food Hygiene Rating Data API.");
-    expect(blurb).toBeInTheDocument();
-    // URLs
-    [
-      "https://ratings.food.gov.uk",
-      "https://www.food.gov.uk/terms-and-conditions"
-    ].forEach(url => {
-      const element = screen.getByText(url);
-      expect(element).toHaveAttribute("href", url);
-    });
+    checkBoilerplate();
     // loading...
     expect(screen.getByText("loading...")).toBeInTheDocument()
   });
@@ -32,3 +36,5 @@ describe("App", () => {
   // TODO test clicking on authority.
 
 });
+
+
