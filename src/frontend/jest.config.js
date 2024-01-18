14e9face1de9ec2bc9f98f31726e67eaae4952a9
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-01-18 12:21:21 +0000

    Add the two eslint plugins mentioned here:
    https://kentcdodds.com/blog/common-mistakes-with-react-testing-library#not-using-testing-library-eslint-plugins
    
    Add @testing-library/jest-dom.
    
    eslint suggests a better way to assert that Table is rendered to nothing.

--- src/frontend/jest.config.js
+++ src/frontend/jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
-  }
+  },
+  setupFilesAfterEnv: [
+    "<rootDir>/setup-jest.ts"
+  ]
 };
