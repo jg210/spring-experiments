be7be937e2329aaf1aba629a467ccb61d715b331
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-01-18 14:23:41 +0000

    Add describe() to App.test.tsx.

--- src/frontend/src/__tests__/App.test.tsx
+++ src/frontend/src/__tests__/App.test.tsx
@@ -1,10 +1,14 @@
 import { render } from '@testing-library/react'
 import App from '../App';
 
-it('is run with correct node version', () => {
-  expect(process.versions.node).toEqual("20.10.0");
-});
+describe("App", () => {
 
-it('renders without crashing', () => {
-  render(<App />);
+  it('is run with correct node version', () => {
+    expect(process.versions.node).toEqual("20.10.0");
+  });
+
+  it('renders without crashing', () => {
+    render(<App />);
+  });
+
 });
