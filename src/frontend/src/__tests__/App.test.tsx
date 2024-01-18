c0e063bd41ac6e5f5d67f702796ec02ecc9a38ac
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-01-18 16:06:18 +0000

    Refactoring and comments.

--- src/frontend/src/__tests__/App.test.tsx
+++ src/frontend/src/__tests__/App.test.tsx
@@ -10,9 +10,13 @@ describe("App", () => {
   it('renders correctly while loading', () => {
     render(<App/>);
     const heading = screen.getByRole("heading");
+    // heading
     expect(heading).toHaveClass("App-title");
     expect(heading).toHaveTextContent("FSA Food Hygiene Ratings");
-    expect(screen.getByText("The information provided here is based on data from the Food Standards Agency UK Food Hygiene Rating Data API.")).toBeInTheDocument();
+    // blurb
+    const blurb = screen.getByText("The information provided here is based on data from the Food Standards Agency UK Food Hygiene Rating Data API.");
+    expect(blurb).toBeInTheDocument();
+    // URLs
     [
       "https://ratings.food.gov.uk",
       "https://www.food.gov.uk/terms-and-conditions"
@@ -20,6 +24,7 @@ describe("App", () => {
       const element = screen.getByText(url);
       expect(element).toHaveAttribute("href", url);
     });
+    // loading...
     expect(screen.getByText("loading...")).toBeInTheDocument()
   });
 
