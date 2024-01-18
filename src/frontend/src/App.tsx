0d93403673d179c28c93db291de5564e1f228d63
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-01-18 15:12:42 +0000

    Assert https links are used in href attributes.

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -24,7 +24,7 @@ class App extends Component<Props,State> {
                 </header>
                 <div>
                     <p>The information provided here is based on data from the Food Standards Agency UK Food Hygiene Rating Data API.</p>
-                    <p><a href="https://ratings.food.gov.uk/">https://ratings.food.gov.uk</a></p>
+                    <p><a href="https://ratings.food.gov.uk">https://ratings.food.gov.uk</a></p>
                     <p><a href="https://www.food.gov.uk/terms-and-conditions">https://www.food.gov.uk/terms-and-conditions</a></p>
                 </div>
                 <Authorities
--- src/frontend/src/__tests__/App.test.tsx
+++ src/frontend/src/__tests__/App.test.tsx
@@ -9,11 +9,18 @@ describe("App", () => {
 
   it('renders correctly while loading', () => {
     render(<App/>);
-    const heading = screen.getByRole("heading");
-    expect(heading).toHaveTextContent("FSA Food Hygiene Ratings");
-    expect(screen.getByText("https://ratings.food.gov.uk")).toBeInTheDocument();
-    expect(screen.getByText("https://www.food.gov.uk/terms-and-conditions")).toBeInTheDocument();
+    expect(screen.getByRole("heading")).toHaveTextContent("FSA Food Hygiene Ratings");
+    [
+      "https://ratings.food.gov.uk",
+      "https://www.food.gov.uk/terms-and-conditions"
+    ].forEach(url => {
+      const element = screen.getByText(url);
+      expect(element).toHaveAttribute("href", url);
+    });
     expect(screen.getByText("loading...")).toBeInTheDocument()
   });
+
+  // TODO test loading of URLs.
+  // TODO test clicking on authority.
 
 });
