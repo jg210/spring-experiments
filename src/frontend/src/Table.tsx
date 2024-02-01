c2e86a2b6d8127869a024c0ad67ca3c46edc648e
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-02-01 13:34:35 +0000

    Convert Table from class to function component.

--- src/frontend/src/Table.tsx
+++ src/frontend/src/Table.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import {
     ratingsPercentages,
@@ -6,82 +6,64 @@ import {
     RatingPercentage
 } from './FSA';
 
-const LOADING_STATE : State = {
-    scores: null
-};
-
 interface Props {
     localAuthorityId: number | null;
 }
 
-interface State {
-    scores: RatingPercentage[] | null
-}
-
 // Table showing percentage of establishments with each rating.
-export class Table extends Component<Props,State> {
+export const Table = ({ localAuthorityId }: Props) => {
 
-    state: State = {
-        ...LOADING_STATE
-    };
-    abortController: AbortController | null = null;
+    const [ scores, setScores ] = useState<RatingPercentage[] | null>(null);
+    const abortControllerRef = useRef<AbortController | null>(null);
 
-    render() {
-        if (this.props.localAuthorityId === null) {
-            return null;
+    useEffect(() => {
+        if (abortControllerRef.current !== null) {
+            abortControllerRef.current.abort();
         }
-        if (this.state.scores === null) {
-            return (
-                <div data-testid="table_loading">loading...</div>
-            );
-        }
-        return (
-            <table className="Table">
-                <thead>
-                    <tr>
-                        <th>Rating</th>
-                        <th>Percentage</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.scores.map((score: RatingPercentage, i) => (
-                        <tr key={i}>
-                            <td>{score.rating}</td>
-                            <td>{Math.round(score.percentage)}%</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        );
-    }
-
-    componentDidUpdate(prevProps: Props) {
-        const localAuthorityId = this.props.localAuthorityId;
-        if (localAuthorityId === prevProps.localAuthorityId) {
-            return;
-        }
-        if (this.abortController !== null) {
-            this.abortController.abort();
-        }
-        this.setState({...LOADING_STATE});
+        setScores(null);
         if (localAuthorityId === null) {
             return;
         }
         const abortController = new AbortController();
-        this.abortController = abortController;
-        fetchEstablishmentsJson(localAuthorityId, this.abortController)
+        abortControllerRef.current = abortController;
+        fetchEstablishmentsJson(localAuthorityId, abortController)
             .then(ratingsPercentages)
             .then((scores: RatingPercentage[]) => {
-                this.setState({ scores });
-                this.abortController = null;
+                setScores(scores);
+                abortControllerRef.current = null;
             })
             .catch(e => {
                 if (!abortController.signal.aborted) {
                     throw e;
                 }
             });
+    }, [localAuthorityId]);
+
+
+    if (localAuthorityId === null) {
+        return null;
     }
-
-}
-
-
+    if (scores === null) {
+        return (
+            <div data-testid="table_loading">loading...</div>
+        );
+    }
+    return (
+        <table className="Table">
+            <thead>
+                <tr>
+                    <th>Rating</th>
+                    <th>Percentage</th>
+                </tr>
+            </thead>
+            <tbody>
+                {scores.map((score: RatingPercentage, i) => (
+                    <tr key={i}>
+                        <td>{score.rating}</td>
+                        <td>{Math.round(score.percentage)}%</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
