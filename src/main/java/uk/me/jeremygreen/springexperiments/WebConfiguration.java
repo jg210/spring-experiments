961949a7b4b8a2df02b3768097f0e6f732c0940e
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-02-06 21:25:50 +0000

    Remove manual static resource handler config since didn't help on AWS.

--- src/main/java/uk/me/jeremygreen/springexperiments/WebConfiguration.java
+++ src/main/java/uk/me/jeremygreen/springexperiments/WebConfiguration.java
@@ -2,7 +2,6 @@ package uk.me.jeremygreen.springexperiments;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,8 +13,4 @@ public class WebConfiguration implements WebMvcConfigurer {
         configurer.setUseTrailingSlashMatch(true);
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:public/");
-    }
 }
