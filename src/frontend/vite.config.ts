634302c9830a4e8b709d2d7cc282a41d0302ccad
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-02-08 20:58:38 +0000

    Make jest pass too. Enable jsdom environment and fix a snapshot file.

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     },
     test: {
         setupFiles: ['./vitest-setup.ts'],
-        globals: true
+        globals: true,
+        environment: 'jsdom'
     }
 });
