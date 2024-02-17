75108b43a17eac4fd0da0548a71644ce50a5ceeb
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-02-17 20:45:58 +0000

    Add Sentry.ErrorBoundary. 100% replaysSessionSampleRate in production since usage will be very low.

--- src/frontend/src/sentry.tsx
+++ src/frontend/src/sentry.tsx
@@ -1,4 +1,5 @@
 import * as Sentry from "@sentry/react";
+import React from "react";
 
 export const sentryInit = () => {
     Sentry.init({
@@ -12,11 +13,25 @@ export const sentryInit = () => {
             }),
         ],
         // Performance Monitoring
-        tracesSampleRate: 1.0, //  Capture 100% of the transactions
-        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracesSampleRate: 1.0, // 100%
+        // Distributed Tracing
         tracePropagationTargets: ["localhost"],
         // Session Replay
-        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+        replaysSessionSampleRate: 1.0,
+        replaysOnErrorSampleRate: 1.0,
     });
+};
+
+type ErrorFallbackProps = {
+    error: Error,
+    componentStack: string
+}
+
+export const ErrorFallback = ({error, componentStack}: ErrorFallbackProps) => {
+    return (
+        <React.Fragment>
+            <div>{error.toString()}</div>
+            <div>{componentStack}</div>
+        </React.Fragment>
+    );
 };
