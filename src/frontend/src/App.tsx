c721b379ddbe7d22c47dab809f6d9ed31ddceb1b
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-02-17 11:29:19 +0000

    Fix mobile layout.

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -10,7 +10,7 @@ export const App  = () => {
             <header className="App-header">
                 <h1 className="App-title">FSA Food Hygiene Ratings</h1>
             </header>
-            <div>
+            <div className="App-blurb">
                 <p>The information provided here is based on data from the Food Standards Agency UK Food Hygiene Rating Data API.</p>
                 <p><a href="https://ratings.food.gov.uk">https://ratings.food.gov.uk</a></p>
                 <p><a href="https://www.food.gov.uk/terms-and-conditions">https://www.food.gov.uk/terms-and-conditions</a></p>
