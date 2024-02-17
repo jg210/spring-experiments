0e2edf2287cf96ef94ebb253cbdd9eea828d620e
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-02-17 17:09:34 +0000

    Start to configure @sentry/react.

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -3,6 +3,9 @@ import './index.css';
 import { App } from './App';
 import { createStore } from './store';
 import { Provider } from 'react-redux';
+import { sentryInit } from './sentry';
+
+sentryInit(); // crash reporting
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
