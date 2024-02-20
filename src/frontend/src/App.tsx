4fa644c64d2d72d40984463b60c9b002ea979cc4
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-02-20 11:55:14 +0000

    Add consent UI for cookies and sentry.

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -3,24 +3,28 @@ import './App.css';
 import { Authorities } from './Authorities';
 import { Table } from './Table';
 import { sentryRelease } from './sentry';
+import CookieConsent from 'react-cookie-consent';
 
 export const App  = () => {
     const [ localAuthorityId, setLocalAuthorityId ] = useState<number|null>(null);
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1 title={sentryRelease} className="App-title">FSA Food Hygiene Ratings</h1>
-            </header>
-            <div data-testid="blurb" className="App-blurb">
-                <p>The information provided here is based on data from the Food Standards Agency UK Food Hygiene Rating Data <a href="https://ratings.food.gov.uk">API</a>.</p>
-                <p><a href="https://www.food.gov.uk/terms-and-conditions">https://www.food.gov.uk/terms-and-conditions</a></p>
+        <>
+            <div className="App">
+                <header className="App-header">
+                    <h1 title={sentryRelease} className="App-title">FSA Food Hygiene Ratings</h1>
+                </header>
+                <div data-testid="blurb" className="App-blurb">
+                    <p>The information provided here is based on data from the Food Standards Agency UK Food Hygiene Rating Data <a href="https://ratings.food.gov.uk">API</a>.</p>
+                    <p><a href="https://www.food.gov.uk/terms-and-conditions">https://www.food.gov.uk/terms-and-conditions</a></p>
+                </div>
+                <Authorities
+                    onClick={setLocalAuthorityId}
+                />
+                { localAuthorityId != null && <Table
+                    localAuthorityId={localAuthorityId}
+                /> }
             </div>
-            <Authorities
-                onClick={setLocalAuthorityId}
-            />
-            { localAuthorityId != null && <Table
-                localAuthorityId={localAuthorityId}
-            /> }
-        </div>
+            <CookieConsent>This site uses cookies. It sends analytics and crash reports to <a className="cookieConsentLink" href="https://sentry.io">sentry</a>.</CookieConsent>
+        </>
     );
 };
