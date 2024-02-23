57e1e4430060d0bfb0a71e2f818142219a618bbf
Jeremy Green <20728342+jg210@users.noreply.github.com>
2024-02-23 17:44:41 +0000

    Add cypress end-to-end test for Document-Policy: js-profiling header.

--- src/frontend/cypress/e2e/basic.cy.ts
+++ src/frontend/cypress/e2e/basic.cy.ts
@@ -3,4 +3,11 @@ describe('basic tests', () => {
     cy.visit('/');
     cy.title().should('eq', 'FSA Food Hygiene Ratings');
   });
+  it('has Document-Policy: js-profiling header', () => {
+    cy.request({url: '/'}).then((response) => {
+      const headers = response.headers;
+      // cypress lowercases the headers! https://github.com/cypress-io/cypress/issues/2879
+      expect(headers['document-policy']).to.equal('js-profiling');
+    });
+  });
 });
